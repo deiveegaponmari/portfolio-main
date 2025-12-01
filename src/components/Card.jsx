bc0d38a73e88fbf92ec758c7e90922ecb60b75b7
deiveegaponmari <ponmarimdeiveega@gmail.com>
2025-12-01 11:55:16 +0530

    changes added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ function Card() {
     const data = [
         {
             id: 1,
-            image: fullstack,
+            image: "https://res.cloudinary.com/djlxdttvr/image/upload/v1764570015/boliviainteligente-IPIwk-Ox1A0-unsplash_c9dn2n.jpg",
             title: "Social Media Platform",
             description: "Developed a full-stack social media platform with real-time chat, posts, comments, likes, and friend requests using React, Node.js, Express, and MongoDB.",
             language: ["HTML,CSS,REACT", "NodeJs", "ExpressJs", "MongoDb"],
@@ -17,29 +17,29 @@ function Card() {
         },
          {
             id: 2,
-            image: fullstack,
+            image: "https://res.cloudinary.com/djlxdttvr/image/upload/v1764570032/church-of-the-king-j9jZSqfH5YI-unsplash_jcuwet.jpg",
             title: "Sri Nataraja School App",
             description: "A feature-rich school management application designed for Sri Nataraja School. The app includes real-time announcements, photo and video uploads, and a feedback module for better communication between school and users. The system is fully responsive and built using React for the frontend and Node.js, Express, and MongoDB for backend and database operations.",
-            language: ["HTML,CSS,Tailwind css,REACT", "NodeJs", "ExpressJs", "MongoDb"],
+            language: ["HTML,CSS,TAILWIND CSS,REACT", "NodeJs", "ExpressJs", "MongoDb"],
             frontendlink: "https://github.com/deiveegaponmari/primary-school",
             backendcode: "https://github.com/deiveegaponmari/backend-school",
             demo: "https://myprimaryschoolapp.netlify.app/"
         },
         {
             id: 3,
-            image: web,
+            image: "https://res.cloudinary.com/djlxdttvr/image/upload/v1764570042/farhat-altaf-8Kooarp0aWY-unsplash_heotik.jpg",
             title: "Weather App",
             description: "This Weather App allows users to check the real-time weather conditions of any location using a city search. It displays key weather information such as temperature, humidity, wind speed, and weather status using icons for better clarity. The app fetches accurate live weather data from a public API (OpenWeather API) and updates results dynamically. Built with modern UI, it delivers a clean and user-friendly experience.",
-            language: ["HTML,CSS,Tailwind css,REACT"],
+            language: ["HTML,CSS,TAILWIND CSS,REACT"],
             frontendlink: "https://github.com/deiveegaponmari/weatherapp",
             demo: "https://weathermyproject.netlify.app/"
         },
         {
             id: 4,
-            image: develop,
+            image: "https://res.cloudinary.com/djlxdttvr/image/upload/v1764570057/martin-martz-CiQ2qEYVo5U-unsplash_l7s7fn.jpg",
             title: "To Do List App",
             description: "A minimal and responsive To-Do List application that helps users manage tasks efficiently. Users can add new tasks, mark completed items, edit them, and delete tasks once done. Built using React and Tailwind CSS with smooth UI interactions for an organized workflow.",
-            language: ["HTML,CSS,Tailwind css,REACT"],
+            language: ["HTML,CSS,TAILWIND CSS,REACT"],
             frontendlink: "https://github.com/deiveegaponmari/todolist",
             demo: "https://todolisttrackerapp.netlify.app/"
         }
